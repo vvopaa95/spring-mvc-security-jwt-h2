b167631f199d787feabc43c5c23f8d695225249e
vvopaa95 <chylikinmaksim@gmail.com>
2019-09-02 01:08:46 +0300

    - projection (class based) - spring data features

--- src/main/java/com/example/mvctpl/user/UserController.java
+++ src/main/java/com/example/mvctpl/user/UserController.java
@@ -9,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URISyntaxException;
 import java.util.Optional;
 
 @RestController
