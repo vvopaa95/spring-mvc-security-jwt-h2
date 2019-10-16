1a77f429a9c65ca882ca00f02790be715f6b9ea7
vvopaa95 <chylikinmaksim@gmail.com>
2019-10-16 12:18:49 +0300

    + Jackson hibernate 5: error while serializing lazy init entity (proxy)

---
+++ src/main/java/com/example/mvctpl/core/WebConfig.java
@@ -0,0 +1,28 @@
+package com.example.mvctpl.core;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurationSupport {
+    public MappingJackson2HttpMessageConverter jacksonMessageConverter(){
+        MappingJackson2HttpMessageConverter messageConverter = new  MappingJackson2HttpMessageConverter();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new Hibernate5Module());
+        messageConverter.setObjectMapper(mapper);
+        return messageConverter;
+
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(jacksonMessageConverter());
+        super.configureMessageConverters(converters);
+    }
+}
