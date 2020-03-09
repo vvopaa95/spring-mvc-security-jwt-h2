306d72b0fc706887f09f2979fc59925239a6d643
maksim_chylikin@epam.com <Kjkrf[f[f[f178>
2020-03-09 23:40:00 +0300

    error with lazy init

--- src/main/java/com/example/mvctpl/user/User.java
+++ src/main/java/com/example/mvctpl/user/User.java
@@ -20,7 +20,7 @@ public class User extends AuditEntity {
   private String username;
   private String password;
 
-  @ManyToMany
+  @ManyToMany(fetch = FetchType.EAGER)
   @JoinTable(
     name = "user_role",
     joinColumns = @JoinColumn(name = "user_id"),
